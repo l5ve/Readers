2c8bd28276f3ca4d8000c9953159cedc701af8d5
Shinyeong97 <shinyeong.yun@gmail.com>
2019-09-21 15:49:24 +0900

    Build basic files and permission check
    Build basic source files such as strings, dimens, colors and build permission check function

---
+++ android/app/src/main/java/com/capstone/readers/IndexActivity.java
@@ -0,0 +1,16 @@
+package com.capstone.readers;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IndexActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+    }
+}
