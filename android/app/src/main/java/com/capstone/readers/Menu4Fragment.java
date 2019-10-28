2eb8889f8d5ba99144b9587a690b947d23410cf7
Shinyeong97 <shinyeong.yun@gmail.com>
2019-10-28 18:36:07 +0900

    Update Ask by email function on setting page
    Update Ask by email function on setting page

--- android/app/src/main/java/com/capstone/readers/Menu4Fragment.java
+++ android/app/src/main/java/com/capstone/readers/Menu4Fragment.java
@@ -1,19 +1,46 @@
 package com.capstone.readers;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.w3c.dom.Text;
+
 public class Menu4Fragment extends Fragment {
+    private TextView settingLoginBtn;
+    private TextView settingHideBtn;
+    private TextView settingNoticeBtn;
+    private TextView settingAskBtn;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_menu4, container, false);
+        View fv = inflater.inflate(R.layout.fragment_menu4, container, false);
+
+        settingLoginBtn = (TextView) fv.findViewById(R.id.setting_login_btn);
+        settingHideBtn = (TextView) fv.findViewById(R.id.setting_hide_btn);
+        settingNoticeBtn = (TextView) fv.findViewById(R.id.setting_notice_btn);
+        settingAskBtn = (TextView) fv.findViewById(R.id.setting_ask_btn);
+
+        settingAskBtn.setOnClickListener(new TextView.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.setType("plain/Text");
+                email.putExtra(Intent.EXTRA_EMAIL, "readers.cau@gmail.com");
+                email.putExtra(Intent.EXTRA_SUBJECT, "[Ask]");
+                email.putExtra(Intent.EXTRA_TEXT, "문의 내용을 작성해주세요.\n");
+                email.setType("message/rfc822");
+                startActivity(email);
+            }
+        });
+        return fv;
     }
 }
