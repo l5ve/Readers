7d2d0b512716703524b4ad4d3524476dff46da99
Shinyeong97 <shinyeong.yun@gmail.com>
2019-11-18 18:49:07 +0900

    Update for block list

--- android/app/src/main/java/com/capstone/readers/Block/BlockListAdapter.java
+++ android/app/src/main/java/com/capstone/readers/Block/BlockListAdapter.java
@@ -3,6 +3,7 @@ package com.capstone.readers.Block;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -13,8 +14,12 @@ import android.view.View;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.capstone.readers.MyApp;
 import com.capstone.readers.R;
+import com.capstone.readers.RetrofitClient;
 import com.capstone.readers.ServiceApi;
+import com.capstone.readers.item.UserToonData;
+import com.capstone.readers.lib.MyToast;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -23,11 +28,19 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class BlockListAdapter extends RecyclerView.Adapter<BlockListAdapter.ViewHolder> {
     Context context;
     private ArrayList<BlockCard> mDataset;
     private Bitmap bitmap;
     private ServiceApi service;
+    private String toon_id;
+    private boolean unblocked;
+    private int thispos;
 
     public BlockListAdapter(Context context, ArrayList<BlockCard> Dataset) {
         this.context = context;
@@ -54,16 +67,36 @@ public class BlockListAdapter extends RecyclerView.Adapter<BlockListAdapter.View
             mLinearLayout.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-
-
+                    unblock();
                 }
             });
         }
     }
 
-    public void block() {
+    public void unblock() {
+        String user_id = ((MyApp) context.getApplicationContext()).getUser_id();
+        UserToonData data = new UserToonData(user_id, toon_id);
+        service.unblock(data).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.code() == 200) {
+                    Log.d("BlockListAdapter", "unblock: " + context.getString(R.string.unblock_success));
+                    MyToast.s(context, context.getString(R.string.unblock_success));
+                    mDataset.remove(thispos);
+                    notifyDataSetChanged();
+                }
+                else {
+                    Log.d("BlockListAdapter", "unblock: " + context.getString(R.string.unblock_fail));
+                    MyToast.s(context, context.getString(R.string.unblock_fail));
+                }
+            }
 
-
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.e("BlockListAdapter", "unblock: " + context.getString(R.string.toon_server_error));
+                MyToast.s(context, context.getString(R.string.toon_server_error));
+            }
+        });
     }
 
     @Override
@@ -79,6 +112,7 @@ public class BlockListAdapter extends RecyclerView.Adapter<BlockListAdapter.View
     @Override
     public void onBindViewHolder(BlockListAdapter.ViewHolder holder, int  position) {
         final int pos = position;
+        thispos = position;
         Thread mThread = new Thread(){
             @Override
             public void run() {
@@ -113,6 +147,9 @@ public class BlockListAdapter extends RecyclerView.Adapter<BlockListAdapter.View
         holder.mPlatform.setText(mDataset.get(position).getPlatform());
         holder.mTitle.setText(mDataset.get(position).getTitle());
         holder.mAuthor.setText(mDataset.get(position).getAuthor());
+        toon_id = mDataset.get(position).getToon_id();
+        unblocked = false;
+        service = RetrofitClient.getClient().create(ServiceApi.class);
     }
 
     @Override
